1a165a7a979c5d35e4eb23abb96ae98f012eb379
Andre Ellis Jr <andre.ellis@precisionplanting.com>
2023-10-05 13:27:40 -0500

    created email template and email forwarding

--- src/components/EventNotes.tsx
+++ src/components/EventNotes.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { supabase } from "../supabase/client";
 import { useParams } from "react-router-dom";
 import { useAuth } from "./context/AuthProvider";
+import emailjs from "@emailjs/browser";
 
 type FormData = {
   notes: string;
@@ -34,6 +35,7 @@ const EventNotes = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    console.log("email section of the handlesubmit");
     try {
       const { error } = await supabase
         .from("events")
@@ -41,12 +43,86 @@ const EventNotes = () => {
           {
             notes: formData.notes,
             rejection_notes: formData.rejection_notes,
-            status: "approved",
           },
         ])
         .eq("id", eventID)
         .select();
       setMessage("Updated!");
+
+      // Sending email
+      const templateParams = {
+        from_name: "Admin",
+        to_name: event?.speaker,
+        reply_to: event?.email,
+        message: `You're keynote has been approved! Looking forward to hearing from you about ${event?.topic}! Here's a link to your keynote https://kinote.netlify.app/events/${event?.id}. Don't forget to have fun!`,
+      };
+
+      emailjs
+        .send(
+          "service_ck37ahe",
+          "template_k9uowyj",
+          templateParams,
+          "4WRu2iRJgACLhXwZn"
+        )
+        .then(
+          (response) =>
+            console.log("Successful Email", response.status, response.text),
+          (err) => {
+            console.log("Failed, Email did not send", err);
+          }
+        );
+
+      if (error) {
+        setError("Looks like something went wrong. Try again");
+        console.log(error);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleRejection = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    console.log("email section of the handle rejection");
+    try {
+      const { error } = await supabase
+        .from("events")
+        .update([
+          {
+            notes: formData.notes,
+            rejection_notes: formData.rejection_notes,
+            status: "rejected",
+          },
+        ])
+        .eq("id", eventID)
+        .select();
+      setMessage("Updated!");
+
+      // Sending email
+      const templateParams = {
+        from_name: "Admin",
+        to_name: event?.speaker,
+        reply_to: event?.email,
+        message: `You're keynote has been rejected! Here are the reasons: (${event?.rejection_notes}). Please forward this email to keynote@precisionplanting.com with your corrections and someone will get ahold of shortly. Event: https://kinote.netlify.app/events/${event?.id}.`,
+      };
+
+      emailjs
+        .send(
+          "service_ck37ahe",
+          "template_4qn7aj9",
+          templateParams,
+          "4WRu2iRJgACLhXwZn"
+        )
+        .then(
+          (response) =>
+            console.log("Successful Email", response.status, response.text),
+          (err) => {
+            console.log("Failed, Email did not send", err);
+          }
+        );
+
       if (error) {
         setError("Looks like something went wrong. Try again");
         console.log(error);
@@ -115,7 +191,11 @@ const EventNotes = () => {
             <p>
               Input your name and the notes you'd like to add to the request.
             </p>
-            <form className="flex flex-col" onSubmit={handleSubmit}>
+            <form
+              className="flex flex-col"
+              onSubmit={handleSubmit}
+              id="submitForm"
+            >
               <textarea
                 name="notes"
                 id="notes"
@@ -142,7 +222,8 @@ const EventNotes = () => {
                 </button>
                 <button
                   type="button"
-                  className="bg-[#AC0000] rounded-lg px-4 py-2 hover:bg-[#174828] transition text-white font-bold"
+                  onClick={handleRejection}
+                  className="bg-[#AC0000] rounded-lg px-4 py-2 hover:bg-[#8e0000] transition text-white font-bold"
                 >
                   REJECT
                 </button>
